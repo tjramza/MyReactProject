63c704bedac8e7c4d5d07ccf7767712f9409b83c
tjramza <78152779+tjramza@users.noreply.github.com>
2022-01-12 22:34:37 +0900

    分割代入

--- src/index.js
+++ src/index.js
@@ -8,13 +8,58 @@ document.getElementById("app").innerHTML = `
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
 </div>
 `;
-const name = "taji";
-const age = "31";
+//const name = "taji";
+// //const age = "31";
 
-//従来の方法
-const message1 = "私の名前は" + name + "です。年齢は" + age + "です";
-console.log(message1);
+// //従来の方法
+// const message1 = "私の名前は" + name + "です。年齢は" + age + "です";
+// console.log(message1);
 
-//テンプレート文字列を用いた方法
-const message2 = `私の名前は${name}です。年齢は${age}です。`;
-console.log(message2);
+// //テンプレート文字列を用いた方法
+// const message2 = `私の名前は${name}です。年齢は${age}です。`;
+// console.log(message2);
+
+// //アロー関数
+// //従来
+// function func1(str) {
+//   return str;
+// }
+// console.log(func1("funk1です"));
+// //アロー関数
+// const func2 = (str) => {
+//   return str;
+// };
+// console.log(func2("funk2です"));
+// const func3 = (str) => {
+//   return str;
+// };
+// console.log(func3("funk3です"));
+// const func4 = (str) => str;
+// console.log(func4("funk4です"));
+
+// const func5 = (num1, num2) => {
+//   return num1 + num2;
+// };
+// console.log(func5(10, 20));
+
+// //分割代入
+// const myProfile = {
+//   name: "taji",
+//   age: 31
+// };
+
+// //const message3 = `名前は${myProfile.name}です。年齢は${myProfile.age}です。`;
+// //console.log(message3);
+
+// const { name, age } = myProfile;
+// const message4 = `名前は${name}です。年齢は${age}です。`;
+// console.log(message4);
+
+const myProfile = ["taji", 31];
+
+// const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}です。`;
+// console.log(message3);
+
+const [name, age] = myProfile;
+const message4 = `名前は${name}です。年齢は${age}です。`;
+console.log(message4);
