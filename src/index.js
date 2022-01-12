41d34974101775bef9e5a210160af8816fa0702f
tjramza <78152779+tjramza@users.noreply.github.com>
2022-01-12 23:14:39 +0900

    map filter

--- src/index.js
+++ src/index.js
@@ -101,3 +101,33 @@ document.getElementById("app").innerHTML = `
 // arr8[0] = 100;
 // console.log(arr4);
 //配列のコピーは参照渡し
+
+//mapやfilterを使った配列の処理;
+
+const nameArr = ["田中", "山田", "taji"];
+// // for (let index = 0; index < nameArr.length; index++) {
+// //   console.log(nameArr[index]);
+// // }
+
+// // const nameArr2 = nameArr.map((name) => {
+// //   return name;
+// // });
+// // console.log(nameArr2);
+
+nameArr.map((name, index) => console.log(`${index + 1}番目は${name}です。`));
+
+// const numArr = [1, 2, 3, 4, 5];
+// const newNumArr = numArr.filter((num) => {
+//   return num % 2 === 1;
+// })
+// console.log(newNumArr);
+
+const newNameArr = nameArr.map((name) => {
+  if (name === "taji") {
+    return name;
+  } else {
+    return `${name}さん`;
+  }
+});
+
+console.log(newNameArr);
