0eb89ec75215a04b6dffb57bee47b28b54ac07d4
tjramza <78152779+tjramza@users.noreply.github.com>
2022-01-12 22:11:23 +0900

    template

--- src/index.js
+++ src/index.js
@@ -8,3 +8,13 @@ document.getElementById("app").innerHTML = `
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
 </div>
 `;
+const name = "taji";
+const age = "31";
+
+//従来の方法
+const message1 = "私の名前は" + name + "です。年齢は" + age + "です";
+console.log(message1);
+
+//テンプレート文字列を用いた方法
+const message2 = `私の名前は${name}です。年齢は${age}です。`;
+console.log(message2);
